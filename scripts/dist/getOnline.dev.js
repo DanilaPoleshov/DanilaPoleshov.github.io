ed79c1ef7d0af9d18eb8e3adf3fe33c1ffd928d2
DanilaPoleshov <86511365+DanilaPoleshov@users.noreply.github.com>
2021-06-28 23:28:23 +0700

    Add files via upload

---
+++ scripts/dist/getOnline.dev.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// (function getOnline() {
+//   fetch("https://api.vimeworld.ru/online?token=тут мой токен был")
+//     .then((data) => {
+//       return data.json();
+//     })
+//     .then((data) => {
+//       console.log(data["total"]);
+//       document.querySelector(".monitor-mg").innerHTML = data["total"];
+//     });
+//   setTimeout(getOnline, 11000);
+// })();
+function maxOnlineMG(arg) {
+  var online = arg;
+  var max;
+
+  if (online % 1000 == 0) {
+    max = online;
+  } else {
+    max = parseInt(online / 1000) * 1000 + 1000;
+  }
+
+  document.querySelector('.max-online-mg').innerHTML = max;
+  return max;
+}
+
+(function getOnline() {
+  fetch("https://api.vimeworld.ru/online?token=DhBr2H8vfauclCeVuHu2PtBYkTwSotS").then(function (data) {
+    return data.json();
+  }).then(function (data) {
+    var totalOnline = document.querySelector(".monitor-mg").innerHTML = data["total"];
+    var maxOnline = maxOnlineMG(totalOnline);
+    console.log(totalOnline);
+    console.log(maxOnline);
+    document.querySelector('.progress-mg').style.width = Number(totalOnline / maxOnline * 100) + '%';
+  });
+  setTimeout(getOnline, 11000);
+})(); // function getOnline() {
+//   fetch("http://mc.vimeworld.ru/mon/min.json")
+//     .then((data) => data)
+//     .then((data) => {
+//       console.log(data["total"]);
+//       document.querySelector(".monitor-mg").innerHTML = data["total"];
+//     });
+//   setTimeout(getOnline, 11000);
+// }
+// getOnline()
+// document.querySelector('.progress-mg').style.width = document.querySelector('.monitor-mg') / doc
--- scripts/getOnline.js
+++ scripts/getOnline.js
@@ -1,14 +1,55 @@
+// (function getOnline() {
+//   fetch("https://api.vimeworld.ru/online?token=тут мой токен был")
+//     .then((data) => {
+//       return data.json();
+//     })
+//     .then((data) => {
+//       console.log(data["total"]);
+//       document.querySelector(".monitor-mg").innerHTML = data["total"];
+//     });
+
+//   setTimeout(getOnline, 11000);
+// })();
+
+function maxOnlineMG(arg) {
+  let online = arg;
+  let max;
+  if (online % 1000 == 0) {
+    max = online;
+  } else {
+    max = parseInt(online / 1000) * 1000 + 1000;
+  }
+  document.querySelector('.max-online-mg').innerHTML = max;
+  return max;
+
+}
+
 (function getOnline() {
-    fetch('https://api.vimeworld.ru/online?token=DtCrnul2AQXmqVq7xprl14BerySlqLf')
-      .then(data => {
-        return data.json()
-      })
-      .then(data => {
-        console.log(data['total']);
-        document.querySelector('.monitor-mg').innerHTML = data['total']
-      })
+  fetch("https://api.vimeworld.ru/online?token=DhBr2H8vfauclCeVuHu2PtBYkTwSotS")
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      let totalOnline = document.querySelector(".monitor-mg").innerHTML = data["total"];
+      let maxOnline = maxOnlineMG(totalOnline)
+      console.log(totalOnline);
+      console.log(maxOnline);
+      document.querySelector('.progress-mg').style.width = Number(totalOnline / maxOnline * 100) + '%'
+    });
+  setTimeout(getOnline, 11000);
+})();
 
-    setTimeout(getOnline, 11000);
-  })();
+// function getOnline() {
+//   fetch("http://mc.vimeworld.ru/mon/min.json")
+//     .then((data) => data)
+//     .then((data) => {
+//       console.log(data["total"]);
+//       document.querySelector(".monitor-mg").innerHTML = data["total"];
+//     });
 
-document.querySelector('.progress-mg').style.width = document.querySelector('.monitor-mg') / doc
+//   setTimeout(getOnline, 11000);
+// }
+// getOnline()
+
+// document.querySelector('.progress-mg').style.width = document.querySelector('.monitor-mg') / doc
+
